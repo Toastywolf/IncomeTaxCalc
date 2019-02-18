c17efabe2989008d5313e0540c86cd249928e3f0
Leonora Tindall <nora@nora.codes>
2019-02-17 21:45:36 -0600

    Add model and controller objects with tax algo

---
+++ app/src/main/java/code/toastywolf/incometaxcalc/TaxCalculator.java
@@ -0,0 +1,100 @@
+package code.toastywolf.incometaxcalc;
+
+public class TaxCalculator {
+    public static double rate(TaxInformation ti) {
+        if (ti.filing_as == TaxInformation.FILING_SINGLE) {
+            return getSingleRate(ti.income);
+        }
+
+        if (ti.filing_as == TaxInformation.FILING_JOINTLY) {
+            return getJointRate(ti.income);
+        }
+
+        if (ti.filing_as == TaxInformation.FILING_HEAD_OF_HOUSEHOLD) {
+            return getHeadOfHouseholdRate(ti.income);
+        }
+
+        if (ti.filing_as == TaxInformation.FILING_SEPERATELY) {
+            return getSeperateRate(ti.income);
+        }
+
+        return Float.NaN;
+    }
+
+    public static double amount(TaxInformation ti) {
+        double rate = rate(ti);
+        return ti.income * rate;
+    }
+
+    private static double getSingleRate(double income) {
+        if (income <= 9525) {
+            return 0.10;
+        } else if (income <= 38700) {
+            return 0.12;
+        } else if (income <= 82500) {
+            return 0.22;
+        } else if (income <= 157500) {
+            return 0.24;
+        } else if (income <= 200000) {
+            return 0.32;
+        } else if (income <= 500000) {
+            return 0.35;
+        } else {
+            return 0.37;
+        }
+    }
+
+    private static double getSeperateRate(double income) {
+        if (income <= 9525) {
+            return 0.10;
+        } else if (income <= 38700) {
+            return 0.12;
+        } else if (income <= 82500) {
+            return 0.22;
+        } else if (income <= 157500) {
+            return 0.24;
+        } else if (income <= 200000) {
+            return 0.32;
+        } else if (income <= 300000) {
+            return 0.35;
+        } else {
+            return 0.37;
+        }
+    }
+
+    private static double getJointRate(double income) {
+        if (income <= 19050) {
+            return 0.10;
+        } else if (income <= 77400) {
+            return 0.12;
+        } else if (income <= 165000) {
+            return 0.22;
+        } else if (income <= 315000) {
+            return 0.24;
+        } else if (income <= 400000) {
+            return 0.32;
+        } else if (income <= 600000) {
+            return 0.35;
+        } else {
+            return 0.37;
+        }
+    }
+
+    private static double getHeadOfHouseholdRate(double income) {
+        if (income <= 13600) {
+            return 0.10;
+        } else if (income <= 51800) {
+            return 0.12;
+        } else if (income <= 82500) {
+            return 0.22;
+        } else if (income <= 157500) {
+            return 0.24;
+        } else if (income <= 200000) {
+            return 0.32;
+        } else if (income <= 500000) {
+            return 0.35;
+        } else {
+            return 0.37;
+        }
+    }
+}
---
+++ app/src/main/java/code/toastywolf/incometaxcalc/TaxInformation.java
@@ -0,0 +1,16 @@
+package code.toastywolf.incometaxcalc;
+
+public class TaxInformation {
+    public static int FILING_SINGLE = 0;
+    public static int FILING_JOINTLY = 1;
+    public static int FILING_HEAD_OF_HOUSEHOLD = 2;
+    public static int FILING_SEPERATELY = 3;
+
+    public double income;
+    public int filing_as;
+
+    public TaxInformation(double income, int filing_as) {
+        this.income = income;
+        this.filing_as = filing_as;
+    }
+}
